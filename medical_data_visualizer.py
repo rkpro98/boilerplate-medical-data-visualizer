5500ec51ea0c71f0e2d9af1bdd937349b23e9476
Rajish Khatiwada <rkpro98@gmail.com>
2024-07-01 12:03:48 +0000

    first coommit

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['BMI'] = df['weight'] / ((df['height']/100)**2)
+df['overweight'] = (df['BMI'] > 25).astype(int)
+df.drop(columns=['BMI'], inplace=True)
 
 # 3
-
+df['gluc'] = (df['gluc'] > 1).astype(int)
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
